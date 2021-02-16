cdb0532484c08b52f5c097ed8e01ac356e01710e
isaac-cyj <isaac_chan_yong_jie@connect.ite.edu.sg>
2021-02-16 11:24:43 +0800

    updated

--- source/scrapyproject/scrapyproject/spiders/moh_covid19.py
+++ source/scrapyproject/scrapyproject/spiders/moh_covid19.py
@@ -23,16 +23,12 @@ class MohCovid19Spider(scrapy.Spider):
        # i = 0
 
 
-########################  return DOSCORN Level ########################
         yield {"DOSCORN Level" : response.css('div.sfContentBlock tr  ::text').getall()[7]}
 
-########################  retrieve last update  ########################
         lastupdate = response.css('div.sfContentBlock ::text ').getall()[20]
         lastupdate = lastupdate.replace(u'\xa0', u' ')
-        yield{"updated since" : lastupdate}
+        yield{"case updated since" : lastupdate}
 
-######################## return amount of imported cases ########################
-        #remove \xa0 from the string
         importedcases  = response.css('div.sfContentBlock tr  ::text').getall()[11]
         importedcases = importedcases.replace(u'\xa0', u'')
         yield {"Amount of imported cases" : importedcases}
@@ -40,6 +36,19 @@ class MohCovid19Spider(scrapy.Spider):
         yield{"summary" : response.css('div.sfContentBlock ::text').getall()[26]}
         yield {"Active Cases" : response.css('div.sfContentBlock tr  ::text').getall()[16]}
         yield {"Discharged" : response.css('div.sfContentBlock tr  ::text').getall()[18]}
+        yield {"In community facilities" : response.css('div.sfContentBlock tr  ::text').getall()[20] }
+        yield {"Hospitalised (Stable)" : response.css('div.sfContentBlock tr  ::text').getall()[22]}
+        yield {"Hospitalised (Crictal)" : response.css('div.sfContentBlock tr  ::text').getall()[24]}
+        yield {"Deaths" : response.css('div.sfContentBlock tr  ::text').getall()[27]}
+
+        swabtested = response.css('div.sfContentBlock ::text').getall()[57] +  response.css('div.sfContentBlock ::text').getall()[58]
+        swabtested = swabtested.replace(u'\u00a0',u'')
+        yield {"swab tested since " : swabtested}
+
+        yield {"Total swab tested" : response.css('div.sfContentBlock tr  ::text').getall()[28]}
+        yield{"Average Daily Number Of Swabs Tested Over The Past Week":response.css('div.sfContentBlock tr  ::text').getall()[29]}
+        yield{"Total Swabs Per 1,000,000 Total Population":response.css('div.sfContentBlock tr  ::text').getall()[30]}
+
 
 
 
